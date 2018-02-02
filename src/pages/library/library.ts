6d4ce0b1245a46202de3d3e350081ca63c0f6aa2
Roshan Lakmal <roshanrox911@gmail.com>
2018-02-02 19:44:52 +0530

    Created an interface called quotes.interface.ts and add the data to library.ts

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
+import { Quote } from "../../data/quotes.interface";
+import quotes from '../../data/quotes';
 
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
 export class LibraryPage {
+  quoteCollection: {category: string, quotes: Quote[], icon: string}[];
 
+  ngOnInit(){
+    this.quoteCollection = quotes;
+  }
 }
